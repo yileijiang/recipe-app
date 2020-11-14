67b00e53579230debf77c73fe195fee2f7bc0eb7
yilei jiang <yilei.jiang0@gmail.com>
2020-11-14 21:41:20 +0100

    refactored code

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,12 @@
 class RecipesController < ApplicationController
+
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
   
   def index
     @recipes = Recipe.all
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def new
@@ -20,24 +21,25 @@ class RecipesController < ApplicationController
   end
 
   def edit
-    @recipe = Recipe.find(params[:id])
   end
 
   def update
-    @recipe = Recipe.find(params[:id])
     @recipe.update(recipe_params)
 
     redirect_to recipe_path(@recipe)
   end
 
   def destroy
-    @recipe = Recipe.find(params[:id])
     @recipe.destroy
 
     redirect_to recipes_path
   end
   
   private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
 
   def recipe_params
     params.require(:recipe).permit(:name, :description, tag_ids: [])
